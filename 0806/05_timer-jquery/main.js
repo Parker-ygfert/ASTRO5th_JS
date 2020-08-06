078dacf38e67869c4c590eb411e2d90aca56dbaf
Parker <ygfert@gmail.com>
2020-08-06 22:17:58 +0800

    jquery to vanilla javascript

--- 0806/05_timer-jquery/main.js
+++ 0806/05_timer-jquery/main.js
@@ -2,23 +2,22 @@
 window.addEventListener("DOMContentLoaded", () => {
   let cnt = 0, running = false, interval;
   const button = document.querySelector("button");
-  document.getElementById("running").classList.add("close");
 
-  // debugger
   button.addEventListener("click", () => {
     if (running) {
-      document.getElementById("running").classList.replace("show", "close");
-      // $("#running").slideUp();
+      document.getElementById("running").classList.remove("show");
+      document.getElementById("running").classList.add("close");
       clearInterval(interval);
       document.querySelector("button").textContent = "Start";
       running = false;
     } else {
-      document.getElementById("running").classList.replace("close", "show");
-      // $("#running").slideDown();
+      document.getElementById("running").classList.remove("close");
+      document.getElementById("running").classList.add("show");
       interval = setInterval(() => {
         cnt++;
         document.getElementById("seconds").textContent = (Math.floor(cnt / 10)).toString();
-        $(".dots div:last-child").prependTo($(".dots")) 
+        let last = document.querySelector(".dots div:last-child")
+        document.querySelector(".dots").insertAdjacentElement("afterBegin", last);
       }, 100);
       document.querySelector("button").textContent = "Pause";
       running = true;
